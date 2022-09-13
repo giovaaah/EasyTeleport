eb8bfbafa1e503fd96c0b8599fa4a4095446eba7
Giovanni <108701657+giovaaah@users.noreply.github.com>
2022-09-13 18:44:35 +0100

    Update Main.java

--- EasyCustomTP/src/main/java/org/example/Main.java
+++ EasyCustomTP/src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
-package org.example;
+package github.com.giovaah.easyteleport;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import github.com.giovaah.easyteleport.Commands.Teleport;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EasyTeleport extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+      //START PLUGIN IN CONSOLE
+        System.out.println("Welcome developer! I'm your favourite teleport plugin! ");
+
+        getCommand("tp").setExecutor(new Teleport());
+        getCommand("tpall").setExecutor(new Teleport());
+
+    }
+    @Override
+    public void onDisable() {
+        // SHUTDOWN PLUGIN IN CONSOLE
+
+        System.out.println("Bye bye developer... see you next time");
+
     }
 }
